25402f487427243dc226fe8e4eae08acc8e2cbdd
Mihail Mihaylov <tielass@gmail.com>
2022-11-28 17:37:41 +0100

    Making the models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
+  CATEGORY = []
   belongs_to :user
+  has_many :members, :wishlists
+  validates_numericality_of :price, :greater_than => 0
+  validates :name, :date, :price, presence: true
+  validates :category, inclusion: { in: CATEGORY }
+  has_one_attached :photo
 end
--- db/migrate/20221128150709_create_events.rb
+++ db/migrate/20221128150709_create_events.rb
@@ -3,7 +3,7 @@ class CreateEvents < ActiveRecord::Migration[7.0]
     create_table :events do |t|
       t.string :name
       t.date :date
-      t.string :category
+      t.string :hobbies
       t.float :price
       t.references :user, null: false, foreign_key: true
       t.string :photo
