4e7d344755052114630b9310700341e623bd4c88
Alexey Lebedev <alexeylebedev1995@gmail.com>
2022-11-29 19:38:27 +0200

    making event show page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,8 @@
 class Event < ApplicationRecord
   CATEGORY = []
   belongs_to :user
-  has_many :members, :wishlists
+  has_many :members
+  has_many :wishlistgifts, through: :wishlist
   validates_numericality_of :price, :greater_than => 0
   validates :name, :date, :price, presence: true
   validates :category, inclusion: { in: CATEGORY }
