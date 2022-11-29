ddaabed0fdd89635b8a62ac0205a44e36b8a1652
Mihail Mihaylov <tielass@gmail.com>
2022-11-29 18:11:18 +0100

    more styling

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -1,4 +1,6 @@
 class Gift < ApplicationRecord
+  CATEGORY = []
+  validates :category, inclusion: { in: CATEGORY }
   has_many :wishlistgifts
   validates :name, :price, :description, presence: true
   has_one_attached :photo
