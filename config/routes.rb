8d7cb24f3817f90d17570a3707a0babc9ced5fd7
Paul Sessbruegger <paul.at.home@gmx.de>
2022-11-30 15:31:39 +0100

    profile-page fist steps

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :events
+  get "/profile", to: "pages#profile"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_150955) do
     t.string "photo"
     t.text "description"
     t.string "link"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
