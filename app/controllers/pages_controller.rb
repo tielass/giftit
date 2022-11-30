8836c9af3ebbc2b7a869be7a7c4f94713b9ab2ac
Paul Sessbruegger <paul.at.home@gmx.de>
2022-11-30 20:03:08 +0100

    added a cute little icon

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,6 @@ class PagesController < ApplicationController
 
   def profile
     @events = Event.where(user: current_user).order(start_time: :asc)
-    # @joinedevent = Event.joins(Member.user_id: current_user_id)
-    @joinedevents = Event.joins(:members).where(members: {user: current_user })
+    @joinedevents = Event.joins(:members).where(members: { user: current_user })
   end
 end
