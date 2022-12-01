8a4b37e5431a9a6d38c92a295520858ac63bb951
Paul Sessbruegger <paul.at.home@gmx.de>
2022-12-01 17:27:26 +0100

    invitation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :photo
   has_many :members
   has_many :events
   has_many :voting
-  
+  devise :confirmable, :invitable
   has_many :events, through: :members
 end
