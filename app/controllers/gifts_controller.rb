8b42a47df76e1ff51460c9eefe3868f0b2f93dd5
Eoin Farren <eoinfarren@gmail.com>
2022-12-05 16:01:23 +0100

    Swipe action on gift index

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -2,17 +2,17 @@ class GiftsController < ApplicationController
   def index
     @gifts = Gift.all
     # Filter category on gift index page
-    @hiking_gifts = Gift.where(category: "Hiking").sample(2)
-    @technology_gifts = Gift.where(category: "Technology").sample(2)
+    @hiking_gifts = Gift.where(category: "Hiking").sample(10)
+    @technology_gifts = Gift.where(category: "Technology").sample(10)
     @chocolate_gifts = Gift.where(category: "Chocolate").sample(2)
     @food_gifts = Gift.where(category: "Food").sample(2)
-    @cooking_gifts = Gift.where(category: "Cooking").sample(2)
+    @cooking_gifts = Gift.where(category: "Cooking").sample(10)
     @running_gifts = Gift.where(category: "Running").sample(2)
     @perfume_gifts = Gift.where(category: "Perfume").sample(2)
     @practical_gifts = Gift.where(category: "Practical").sample(2)
     @subscriptions_gifts = Gift.where(category: "Subscriptions").sample(2)
     @wellness_gifts = Gift.where(category: "Wellness").sample(2)
-    @yoga_gifts = Gift.where(category: "Yoga").sample(2)
+    @yoga_gifts = Gift.where(category: "Yoga").sample(10)
     @wildcard_gifts = Gift.where(category: "Wildcard").sample(2)
   end
 
