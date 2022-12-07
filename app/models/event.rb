37246188570782cd5d2f9b3cbdc2ff8cd8f550dc
Eoin Farren <eoinfarren@gmail.com>
2022-12-07 10:36:38 +0100

    require image fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,7 @@ class Event < ApplicationRecord
   has_many :wishlistgifts
   validates_numericality_of :price, :greater_than => 0
   validates :name, :start_time, :price, presence: true
+  validates :photo, presence: true
   has_one_attached :photo
   acts_as_taggable_on :tags
   # acts_as_taggable_on :hobbies
