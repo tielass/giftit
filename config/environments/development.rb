84be9e7b6ef1b213b38ef4d39436891c2db02421
Eoin Farren <eoinfarren@gmail.com>
2022-12-08 15:29:35 +0100

    Price/category fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,6 +75,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
   # Raises error for missing translations.
+  config.active_storage.service = :cloudinary
   # config.i18n.raise_on_missing_translations = true
 
   # Annotate rendered view with file names.
